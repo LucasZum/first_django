e34d7d2d7471893bf584412f4f93b1ec0d591350
LucasZum <lucaspol436@gmail.com>
2023-03-02 21:11:50 -0300

    Views estão ultilizando os dados fornecidos pelo SQLite

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,44 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 def home(req):
     return render(req, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(9)],
+        'recipes': Recipe.objects.filter(
+            is_published=True
+        ).order_by('-id')
     })
 
 
-def recipes(req, id):
+def category(req, category_id):
+
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+    ).order_by('-id')
+
+    if not recipes:
+        return HttpResponse('Not Found', status=404)
+
+    return render(req, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes.first().category.name} | Category'
+    })
+
+
+def recipe(req, id):
+
+    recipe = Recipe.objects.filter(
+        id=id,
+        is_published=True,
+    ).order_by('-id').first()
+
+    if not recipe:
+        return HttpResponse('Nenhuma receita encontrada!', status=404)
+
     return render(req, 'recipes/pages/recipes-view.html', context={
-        'recipe': make_recipe(),
-        'is_recipe_page': True
+        'is_recipe_page': True,
+        'recipe': recipe
     })
